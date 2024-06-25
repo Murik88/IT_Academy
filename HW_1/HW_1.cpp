a37e5128c853e3b79b98d6bc6ee2504757e9c057
Murik88 <murik518@gmail.com>
2024-06-25 17:03:56 +0300

    add file

---
+++ HW_1/HW_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+	constexpr int FOOT_TO_INCH = 12;
+	constexpr float INCH_TO_M = 0.254;
+	constexpr float POUNDS_TO_KG = 2.2;
+	int foot, inch, pounds;
+	double bmi;
+	std::cout << "Enter height (foot then inch): ";
+	std::cin >> foot >> inch;
+	std::cout << "Enter weight: ";
+	std::cin >> pounds;
+	std::cout << "BMI = " << (pounds / POUNDS_TO_KG) / sqrt((foot * FOOT_TO_INCH + inch) * INCH_TO_M);
+	return 0;
+}
